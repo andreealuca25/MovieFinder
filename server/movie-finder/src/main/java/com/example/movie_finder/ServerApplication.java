abefbaf2e2ce8ee5ded435708ee3b49946f1287d
Luca Andreea <lucaandreearaluca@yahoo.com>
2024-08-15 09:58:58 +0300

    feat: use postgres

--- server/movie-finder/src/main/java/com/example/movie_finder/ServerApplication.java
+++ server/movie-finder/src/main/java/com/example/movie_finder/ServerApplication.java
@@ -2,8 +2,10 @@ package com.example.movie_finder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.example.movie_finder"})
 public class ServerApplication {
 
 	public static void main(String[] args) {
